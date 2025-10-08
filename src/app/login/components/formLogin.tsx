3fce7e4a067245e479a7bff4fadb957aeaff9a5d
Nguyễn Vũ Minh Toàn + Gofiber <toannvm@gofiber.vn>
2025-10-08 18:06:05 +0700

    feat update

--- src/app/login/components/formLogin.tsx
+++ src/app/login/components/formLogin.tsx
@@ -4,14 +4,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "@/components/ui/card";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function FormLogin() {
   const [email, setEmail] = useState("");
--- src/app/register/components/formRegister.tsx
+++ src/app/register/components/formRegister.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function RegisterFormPage() {
   const [formData, setFormData] = useState({
